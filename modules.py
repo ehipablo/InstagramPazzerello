4313aee3171696e29713e6af0007fa6827d27c61
ehipablo <41468696+ehipablo@users.noreply.github.com>
2018-08-02 17:52:32 +0200

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,18 @@
+import subprocess, time
+
+def MEDIAID():
+    global urlfoto
+    global urlfoto
+    global mediaID
+    urlfoto = raw_input("[+]Url foto: ")
+    url = ("https://api.instagram.com/oembed/?url="+urlfoto)
+    subprocess.Popen(['xdg-open', url])
+    time.sleep(10)
+
+def USERID():
+    global username
+    global url
+    username = raw_input("[+]Username of the enemy XD: ")
+    url = "https://www.instagram.com/"+username+"?__a=1"
+    subprocess.Popen(['xdg-open', url])
+    time.sleep(10)
